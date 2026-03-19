6f4cfa1ab657863a8ae20b61cd32bf375a7e4eb8
imshahidraza <imshahidraza@gmail.com>
2026-03-20 00:10:16 +0530

    fixed eslint errors

--- frontend/src/pages/Admin.js
+++ frontend/src/pages/Admin.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 const API = 'https://resume-builder-api-i14h.onrender.com';
